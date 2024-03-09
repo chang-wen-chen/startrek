274e9f6f3ba10b5cf966f79b5e5ee9628cff95b1
Chang-Wen <eric81510@gmail.com>
2024-03-09 16:21:42 +0800

    Create bullets, collisions

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import random
 
 # Constant variables
 FPS = 60
-WHITE = (255,255,255)
-GREEN = (0, 255, 0)
-RED = (255, 0 ,0)
+
 WIDTH = 500
 HEIGHT = 600
+
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0 ,0)
+DARKGRAY = (47, 79, 79)
 
 # Initialize game and window
 pygame.init()
@@ -20,7 +23,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,40))
-        self.image.fill(GREEN)
+        self.image.fill(DARKGRAY)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH/2
         self.rect.bottom = HEIGHT - 20
@@ -38,6 +41,11 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
@@ -62,15 +70,37 @@ class Rock(pygame.sprite.Sprite):
             self.speedy = random.randrange(2,10)
             self.speedx = random.randrange(-3,3)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = x
+        self.rect.bottom = y
         
+ 
+    
+    def update(self):
+        self.speedy = -10
+
+        if self.rect.bottom < 0:
+            self.kill()
+        self.rect.y += self.speedy    
 
 all_sprites = pygame.sprite.Group()
+
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
 player = Player()
 
 all_sprites.add(player)
 for i in range(18):
     rock = Rock()
     all_sprites.add(rock)
+    rocks.add(rock)
 
 # Game loop
 running = True
@@ -84,12 +114,26 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     # Update game
     all_sprites.update()
+    # Judge if group elements collide, return crash number
+    hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
+    for hit in hits:
+        r = Rock()
+        all_sprites.add(r)
+        rocks.add(r)
+
+    # Judge trek is hit by rock, game finished
+    hits = pygame.sprite.spritecollide(player, rocks, False)
+    if hits:
+        running = False
 
     # Render
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     all_sprites.draw(screen)
     pygame.display.update()
             
